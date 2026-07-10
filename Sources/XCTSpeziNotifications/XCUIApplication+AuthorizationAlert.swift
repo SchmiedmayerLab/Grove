e05f2052883174961aea4105ab0c4079224d28b7
Lukas Kollmer <hey@lukaskollmer.de>
2026-07-10 09:52:44 +0200

    indicate alert presence

--- Sources/XCTSpeziNotifications/XCUIApplication+AuthorizationAlert.swift
+++ Sources/XCTSpeziNotifications/XCUIApplication+AuthorizationAlert.swift
@@ -26,14 +26,16 @@ extension XCUIApplication {
     ///     Setting this option to `false` will result in the function simply returning and tests continuing without issue,
     ///     if the alert does not appear, e.g. because it already was responded to in the past.
     ///     Defaults to `false`.
+    /// - returns: A boolean value indicating whether the alert appeared.
     @available(tvOS, unavailable)
     @available(macOS, unavailable)
     @available(watchOS, unavailable)
+    @discardableResult
     public func confirmNotificationAuthorization(
         action: NotificationAuthorizationAction = .allow,
         timeout: TimeInterval = 5,
         requireAlertToAppear: Bool = false
-    ) {
+    ) -> Bool {
         let predicate = NSPredicate(format: "label CONTAINS 'Would Like to Send You Notifications'")
         #if os(iOS)
         let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
@@ -41,19 +43,23 @@ extension XCUIApplication {
         if alert.waitForExistence(timeout: timeout) {
             XCTAssert(alert.buttons[action.rawValue].exists)
             alert.buttons[action.rawValue].tap()
+            return true
         } else if requireAlertToAppear {
             // the alert is required to appear within `timeout`, but didn't
             XCTFail("Notification permissions alert did not appear.")
         }
+        return false
         #elseif os(visionOS)
         let notifications = XCUIApplication(bundleIdentifier: "com.apple.RealityNotifications")
         if notifications.scrollViews.staticTexts.element(matching: predicate).waitForExistence(timeout: timeout) {
             XCTAssert(notifications.buttons[action.rawValue].exists)
             notifications.buttons[action.rawValue].tap()
+            return true
         } else if requireAlertToAppear {
             // the alert is required to appear within `timeout`, but didn't
             XCTFail("Notification permissions alert did not appear.")
         }
+        return false
         #endif
     }
 }
