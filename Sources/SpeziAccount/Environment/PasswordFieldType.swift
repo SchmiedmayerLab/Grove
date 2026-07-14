06bce7c296cf9d1db75097bfb0c22a886583bff4
Lukas Kollmer <hey@lukaskollmer.de>
2026-07-14 13:41:01 +0200

    EnvironmentKey

--- Sources/SpeziAccount/Environment/PasswordFieldType.swift
+++ Sources/SpeziAccount/Environment/PasswordFieldType.swift
@@ -50,10 +50,6 @@ extension PasswordFieldType: Sendable, Hashable {}
 
 
 extension EnvironmentValues {
-    private struct PasswordFieldTypeKey: EnvironmentKey {
-        static let defaultValue: PasswordFieldType = .password
-    }
-
     /// The semantic use of a password field.
     ///
     /// ## Topics
