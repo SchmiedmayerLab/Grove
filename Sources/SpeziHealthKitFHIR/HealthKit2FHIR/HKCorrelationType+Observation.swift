1d101056983b7d54f03f51b946c7f517337d2ccd
Lukas Kollmer <hey@lukaskollmer.de>
2026-08-02 18:41:20 -0500

    fix HKSample->FHIR category handling

--- Sources/SpeziHealthKitFHIR/HealthKit2FHIR/HKCorrelationType+Observation.swift
+++ Sources/SpeziHealthKitFHIR/HealthKit2FHIR/HKCorrelationType+Observation.swift
@@ -21,11 +21,7 @@ extension HKCorrelation: FHIRObservationBuildable {
             throw SpeziHealthKitFHIRError.notSupported
         }
         observation.append(codings: correlationTypeMapping.codings)
-        for category in correlationTypeMapping.categories {
-            observation.append(
-                category: CodeableConcept(coding: [category])
-            )
-        }
+        observation.append(categories: correlationTypeMapping.categories.map { CodeableConcept(coding: [$0]) })
         for object in self.objects {
             guard let sample = object as? HKQuantitySample else {
                 throw SpeziHealthKitFHIRError.notSupported
